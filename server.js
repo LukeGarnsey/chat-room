3101ade27a19de0167e31d29aa4b8956f35353b7
Luke Garnsey <lukegarnsey@gmail.com>
2024-02-03 23:41:12 -0500

    socket io setup

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const {Server} = require("socket.io");
 
 const app = express();
 const PORT = process.env.PORT | 3001;
+const ADMIN = "Admin";
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -9,7 +11,63 @@ app.use(express.static("public"));
 
 
 
-
-app.listen(PORT, ()=>{
+const expressServer = app.listen(PORT, ()=>{
   console.log("server active...");
 });
+
+const UsersState = {
+  users:[],
+  setUsers: function(newUsersArray){
+    this.users = newUsersArray
+  }
+}
+
+const io = new Server(expressServer, {
+  cors:{
+    origin: process.env.NODE_ENV === "production"?false:['http://localhost:5500', 'http://127.0.0.1:5500']
+  }
+});
+
+io.on('connection', socket =>{
+  socket.emit('message', "Welcome to Chat Rooms App");
+
+  socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)}: connected}`);
+  socket.on('message', `User ${socket.id.substring(0, 5)}: ${data}`);
+});
+
+function buildMsg(name, text){
+  return {
+    name,
+    text,
+    time: new Intl.DateTimeFormat('default', {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    }).format(new Date())
+  };
+}
+
+function activateUser(id, name, room){
+  const user = {id, name, room};
+  UsersState.setUsers([
+    ...UsersState.users.filter(user=>user.id !== id),
+    user
+  ])
+  return user;
+}
+
+function userLeavesApp(id){
+  UsersState.setUsers(
+    UsersState.users.filter(user => user.id !== id)
+  )
+}
+
+function getUser(id){
+  return UsersState.users.find(user => user.id === id);
+}
+function getUsersInRoom(room){
+  return UsersState.users.filter(user => user.room === room);
+}
+function getAllActiveRooms(){
+  return Array.from(new Set(UsersState.users.map(user => user.room)));
+}
